fd2437d013c0d25692036ccdd04e8b088ac429fd
Ahmed <ahmeddhehek@gmail.com>
2025-11-19 14:05:01 +0300

    anagram-detector-lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        self.sorted_word = sorted(self.word)
+
+    def match(self, candidates):
+        return [
+            candidate for candidate in candidates
+            if sorted(candidate.lower()) == self.sorted_word and candidate.lower() != self.word
+        ]
